c49f0fc5d3ee649f64a6fc6309dfd7bae9fba887
Yasin <162728365+yaseeq@users.noreply.github.com>
2024-06-17 21:15:20 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Database/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ConsoleApp1.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Booking> bookingtable { get; set; }
+}
