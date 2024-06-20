c2aba48bd2eed4c460cb74b6b98d2da360149af2
Yasin <162728365+yaseeq@users.noreply.github.com>
2024-06-20 15:11:54 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Npgsql;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace ConsoleApp1;
+
+public class Booking
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string date { get; set; }
+    public string phone { get; set; }
+}
+
+class Reader
+{
+    static void ClearDatabase(string connString) //очищение бд
+    {
+        using (var conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (var cmd = new NpgsqlCommand("DELETE FROM bookingtable", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            conn.Close();
+        }
+    }
+
+    static string FormatPhoneNumber(string phone) //форматирование номеров
+    {
+        // Удаление всех символов, кроме цифр
+        phone = Regex.Replace(phone, @"\D", "");
+
+        // Если первая цифра 8, заменяем на 7
+        if (phone.StartsWith("8"))
+        {
+            phone = "7" + phone.Substring(1);
+        }
+
+        // оставляем 11 цифр номера
+        if (phone.Length > 11)
+        {
+            phone = phone.Substring(0, 11);
+        }
+
+        //обнуление, если меньше 11 цифр
+        else if (phone.Length < 11)
+        {
+            phone = "0";
+        }
+
+        return phone;
+    }
+
+    static string FormatName(string name) //форматирование имен
+    {
+        name = Regex.Replace(name, "[^а-яА-Яa-zA-Z ]", ""); //убрать все символы кроме букв и пробелов
+        if (!string.IsNullOrEmpty(name) && name[0] == ' ') //убрать пробел в начале
+        {
+            name = name.Substring(1);
+        }
+
+        return name;
+    }
+
+    static void Main()
+    {
+        string connString = "Host=178.208.81.134; Port=5432; Username=postgres; Password=password; Database=postgres;";
+        string csvfile = @"C:\Users\ROOT\Desktop\test1.csv";
+        var listbooking = new List<Booking>();
+        //ClearDatabase(connString);
+
+        fastCSV.ReadFile<Booking>( //чтение csv файла
+            csvfile, // filename
+            true, // has header
+            ',', // delimiter
+            (o, c) => // to object function o : cars object, c : columns array read
+            {
+                var booking = new Booking();
+                booking.id = c[0];
+                booking.name = FormatName(c[10]);
+                booking.date = c[6];
+                booking.phone = FormatPhoneNumber(c[13]);
+                ;
+                listbooking.Add(booking);
+                return true;
+            });
+        foreach (var booking in listbooking)
+
+        {
+            Console.WriteLine($"ID: {booking.id}, Name: {booking.name}, Date: {booking.date}, Phone: {booking.phone}");
+        }
+
+        using (var conn = new NpgsqlConnection(connString)) //перенос в бд
+        {
+            conn.Open();
+            foreach (var booking in listbooking)
+            {
+                using (var cmd = new NpgsqlCommand(
+                           "INSERT INTO bookingtable (id, name, date, phone) VALUES (@id, @name, @date, @phone)", conn))
+                {
+                    //DateTime date = DateTime.ParseExact(booking.date, "dd.M.yyyy HH:mm",
+                       // System.Globalization.CultureInfo.InvariantCulture);
+                    cmd.Parameters.AddWithValue("id", booking.id);
+                    cmd.Parameters.AddWithValue("name", booking.name);
+                    cmd.Parameters.AddWithValue("date", booking.date);
+                    cmd.Parameters.AddWithValue("phone", booking.phone);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            conn.Close();
+        }
+    }
+}
---
+++ WhatsApp/WhatsApp/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Text;
+using Npgsql;
+
+namespace WhatsApp;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url =
+            "https://1103.api.green-api.com/waInstance1103947345/sendMessage/8d9e1f0dd0d54a4d9ba23d9e9f8959c21ea56814176d4954aa";
+        string connString = "Host=178.208.81.134; Port=5432; Username=postgres; Password=password; Database=postgres;";
+
+        using (var conn = new NpgsqlConnection(connString))
+        {
+            var conn2 = new NpgsqlConnection(connString);
+            var connwrite2 = new NpgsqlConnection(connString);
+            var connwrite = new NpgsqlConnection(connString);
+            connwrite.Open();
+            conn.Open();
+            conn2.Open();
+            connwrite2.Open();
+
+            while (true)
+            {
+                var sqlCommand = "SELECT id, name, phone FROM bookingtable WHERE message is null";
+                var command = new NpgsqlCommand(sqlCommand, conn);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader["name"].ToString();
+                        var phone = reader["phone"].ToString();
+                        var id = reader["id"].ToString();
+
+                        if (!string.IsNullOrEmpty(phone))
+                        {
+                            string json = $@"{{
+                            ""chatId"": ""{phone}@c.us"",
+                            ""message"": ""Здравствуйте, {name}, Вы забронировали номер.""
+                        }}";
+
+                            HttpClient client = new HttpClient();
+                            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                            content.Headers.ContentType =
+                                new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                            // Send POST request
+                            HttpResponseMessage response = await client.PostAsync(url, content);
+
+                            // Handle response
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string responseContent = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine($"Message sent to {name} ({phone})");
+                                Console.WriteLine(responseContent);
+
+                                var cmd = new NpgsqlCommand($"UPDATE bookingtable SET message = '1' WHERE id = '{id}'",
+                                    connwrite);
+                                cmd.ExecuteNonQuery();
+
+                            }
+                            else
+                            {
+                                Console.WriteLine(
+                                    $"Failed to send message to {name} ({phone}), status code: {response.StatusCode}");
+                            }
+                        }
+
+                        await Task.Delay(1000);
+                    }
+                }
+
+                var sqlCommand2 = "SELECT id, name, date, phone FROM bookingtable WHERE message2 is null";
+                var command2 = new NpgsqlCommand(sqlCommand2, conn2);
+                using (var reader2 = command2.ExecuteReader())
+                {
+                    while (reader2.Read())
+                    {
+                        var id2 = reader2["id"].ToString();
+                        var name2 = reader2["name"].ToString();
+                        var dateString = reader2["date"].ToString();
+                        var phone2 = reader2["phone"].ToString();
+
+                        if (!string.IsNullOrEmpty(phone2) && DateTime.TryParse(dateString, out var date))
+                        {
+                            var currentTimeUtcPlus8 = DateTime.UtcNow.AddHours(8);
+                            if (date <= currentTimeUtcPlus8)
+                            {
+                                string json = $@"{{
+                            ""chatId"": ""{phone2}@c.us"",
+                            ""message"": ""Здравствуйте, {name2}, Вы выехали из номера, вам всё понравилось?""
+                        }}";
+                                HttpClient client = new HttpClient();
+                                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    string responseContent = await response.Content.ReadAsStringAsync();
+                                    Console.WriteLine($"Message sent to {name2} ({phone2})");
+                                    Console.WriteLine(responseContent);
+
+                                    var updateCmd = new NpgsqlCommand(
+                                        $"UPDATE bookingtable SET message2 = '1' WHERE id = '{id2}'", connwrite2);
+                                    updateCmd.ExecuteNonQuery();
+                                }
+                                else
+                                {
+                                    Console.WriteLine(
+                                        $"Failed to send message to {name2} ({phone2}), status code: {response.StatusCode}");
+                                }
+                            }
+                        }
+
+                        await Task.Delay(1000);
+                    }
+                }
+            }
+        }
+    }
+}
+
